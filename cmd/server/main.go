0dbab23825b064b0623040c0cd7fbdbf40438cd4
Daniel1984 <aciddaniel@gmail.com>
2021-01-10 14:56:30 +0200

    assembling server

---
+++ cmd/server/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/port-scanner/cmd/server/cfg"
+	"github.com/port-scanner/cmd/server/router"
+	"github.com/port-scanner/pkg/gracefulshutdown"
+	"github.com/port-scanner/pkg/server"
+)
+
+func main() {
+	cfg := cfg.New()
+
+	// configure server instance
+	srv := server.
+		New().
+		WithAddr(cfg.GetAPIPort()).
+		WithRouter(router.Get(cfg)).
+		WithErrLogger(cfg.Errlog)
+
+	// start server in separate goroutine so that it doesn't block graceful
+	// shutdown handler
+	go func() {
+		cfg.Infolog.Printf("starting server at %s", cfg.GetAPIPort())
+		if err := srv.Start(); err != nil {
+			cfg.Errlog.Printf("starting server: %s", err)
+		}
+	}()
+
+	// initiate graceful shutdown handler that will listen to api crash signals
+	// and perform cleanup
+	gracefulshutdown.Init(cfg.Errlog, func() {
+		if err := srv.Close(); err != nil {
+			cfg.Errlog.Printf("closing server: %s", err)
+		}
+	})
+}
